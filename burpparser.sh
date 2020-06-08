7eeaa41baa9a5c420f42f5c1e8f0cbd28f307ba4
uzeyirdestan <16destan@gmail.com>
2020-06-09 00:21:52 +0300

    Update burpparser.sh
    Additional editings

--- burpparser.sh
+++ burpparser.sh
@@ -4,7 +4,17 @@ if [ "$#" -lt 1 ]; then
         echo "Default out file is wordlist.txt"
         exit
 elif [ "$#" -eq 1 ]; then
-        cat $1| egrep -iHona "(GET|POST|PUT|HEAD|DELETE|OPTIONS)\ [a-z0-9\?\'\&\=\"\ \/\\]+? HTTP\/[0-9\.]*" | cut -d " " -f3 | sort -u >wordlist.txt
+        echo "Fetching requests in the state file"
+        cat $1| egrep -iHona "(GET|POST|PUT|HEAD|DELETE|OPTIONS)\ [a-z0-9\?\'\&\=\"\ \/\\]+? HTTP\/[0-9\.]*" | cut -d " " -f3 | sort -u >tmp.txt
+        echo "Fetching urls in the src and href attributes"
+        cat $1| egrep -iHona "(href|src)=(\"|\')[a-z0-9\?\&\=\ \/\\\:\_\-]+?*(\"|\')" | cut -d "=" -f2 | sed "s:\"::g" | sed "s:\'::g" | sort -u >>tmp.txt
+        cat tmp.txt | sort -u > wordlist.txt
+        rm tmp.txt
 else
-        cat $1| egrep -iHona "(GET|POST|PUT|HEAD|DELETE|OPTIONS)\ [a-z0-9\?\'\&\=\"\ \/\\]+? HTTP\/[0-9\.]*" | cut -d " " -f3 | sort -u >"$2"
+        echo "Fetching requests in the state file"
+        cat $1| egrep -iHona "(GET|POST|PUT|HEAD|DELETE|OPTIONS)\ [a-z0-9\?\'\&\=\"\ \/\\]+? HTTP\/[0-9\.]*" | cut -d " " -f3 | sort -u >tmp.txt
+        echo "Fetching urls in the src and href attributes"
+        cat $1| egrep -iHona "(href|src)=(\"|\')[a-z0-9\?\&\=\ \/\\\:\_\-]+?*(\"|\')" | cut -d "=" -f2 | sed "s:\"::g" | sed "s:\'::g" | sort -u >>tmp.txt
+        cat tmp.txt | sort -u >"$2"
+        rm tmp.txt
 fi
